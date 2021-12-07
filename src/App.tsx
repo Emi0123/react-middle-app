1207bad7b645bb2174077a311302743d785c75a6
Emi0123 <92375681+Emi0123@users.noreply.github.com>
2021-12-07 23:34:44 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import theme from "./theme/theme";
+import { Button, ChakraProvider } from "@chakra-ui/react";
+
+export default function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <Button colorScheme="teal">ボタン</Button>
+      <p>aaaa</p>
+    </ChakraProvider>
+  );
+}
