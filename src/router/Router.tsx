902ecfbba9d71aec3103c4d6929b8f3323b1f49b
Emi0123 <92375681+Emi0123@users.noreply.github.com>
2021-12-12 00:41:42 +0900

    addHeader

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -6,15 +6,51 @@ import { UserManagement } from "../components/pages/UserManagement";
 import { Home } from "../components/pages/Home";
 import { Setting } from "../components/pages/Setting";
 import { Page404 } from "../components/pages/Page404";
+import { HeaderLayout } from "../components/templates/HeadeLayout";
 
 export const Router: VFC = memo(() => {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/user_management" element={<UserManagement />} />
-      <Route path="/setting" element={<Setting />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/*" element={<Page404 />} />
+      <Route
+        path="/"
+        element={
+          <HeaderLayout>
+            <Login />
+          </HeaderLayout>
+        }
+      />
+      <Route
+        path="/user_management"
+        element={
+          <HeaderLayout>
+            <UserManagement />
+          </HeaderLayout>
+        }
+      />
+      <Route
+        path="/setting"
+        element={
+          <HeaderLayout>
+            <Setting />
+          </HeaderLayout>
+        }
+      />
+      <Route
+        path="/home"
+        element={
+          <HeaderLayout>
+            <Home />
+          </HeaderLayout>
+        }
+      />
+      <Route
+        path="*"
+        element={
+          <HeaderLayout>
+            <Page404 />
+          </HeaderLayout>
+        }
+      />
     </Routes>
   );
 });
